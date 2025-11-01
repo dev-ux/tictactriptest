e472ca970ca5dbb206db4757feddf5aa637628a5
Kassi_0958 <marvinjunior0958@gmail.com>
2025-11-01 07:24:09 +0100

    Augmentation de la limite de taille à 100 Mo dans le code source

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.text({ 
   type: 'text/plain',
-  limit: '50mb' // Augmente la limite à 10 Mo
+  limit: '100mb' // Augmente la limite à 10 Mo
 }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
 
 // Routes
 app.use('/api/token', authRouter);
